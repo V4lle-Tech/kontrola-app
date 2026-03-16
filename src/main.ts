ba1e267e0a5f58bbb0b500c3de8958301489fb50
default <antonio@v4lle.tech>
2026-03-16 00:01:06 +0000

    feat(scaffold): configure PrimeVue Aura preset with orange primary theme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primevue/themes'
 import Aura from '@primevue/themes/aura'
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
@@ -15,9 +16,27 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+const KontrolaPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}',
+    },
+  },
+})
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: KontrolaPreset,
     options: {
       prefix: 'p',
       darkModeSelector: '.dark',
